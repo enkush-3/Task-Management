f9c28b7d25f3b6f5e66ab07e598e093f656b7be1
enkush-3 <enkush.leymo@gmail.com>
2026-06-10 16:38:46 +0800

    Api method-ийн бүтэц

--- backend/modules/workspace/routes.js
+++ backend/modules/workspace/routes.js
@@ -1,29 +1,20 @@
 import { Router } from "express";
 import { Auth } from "../auth/auth.js";
-import {
-    getWorkspace,
-    getAllWorkspace,
-    getLazyWorkspace,
-} from "./service/get/get.js";
-import { createWorkspace, leaveWorkspace } from "./service/post/post.js";
-import { patchWorkspace, restoreWorkspace } from "./service/patch/patch.js";
-import {
-    hardDeleteWorkspace,
-    softDeleteWorkspace,
-} from "./service/delete/delete.js";
+import { getAllWorkspace, getLazyWorkspace } from "./service/get.js";
+import { createWorkspace } from "./service/post.js";
+import { patchWorkspace } from "./service/patch.js";
+import { hardDeleteWorkspace } from "./service/delete.js";
+
 const workspace = Router();
 
+
 workspace.post("/create", Auth, createWorkspace);
-workspace.post("/leave/:workspaceId", Auth, leaveWorkspace);
 
 workspace.patch("/update/:workspaceId", Auth, patchWorkspace);
-workspace.patch("/restore/:workspaceId", Auth, restoreWorkspace);
 
 workspace.get("/getall", Auth, getAllWorkspace);
 workspace.get("/getlazy/:workspaceId", Auth, getLazyWorkspace);
-workspace.get("/:workspaceId", Auth, getWorkspace);
 
-workspace.delete(":workspaceId", Auth, softDeleteWorkspace);
 workspace.delete("/hard/:workspaceId", Auth, hardDeleteWorkspace);
 
 export default workspace;
