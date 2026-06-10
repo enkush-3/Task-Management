ddef83fcebf666cf436352d5d5722ec093fbef09
enkush-3 <enkush.leymo@gmail.com>
2026-06-10 16:54:52 +0800

    Task-ийн frontend хэсэсгийн бүтэц

---
+++ frontend/src/modal/task.js
@@ -0,0 +1,13 @@
+export const TaskFields = {
+    _id: 'string',
+    createdBy: 'string',
+    workspaceId: 'string',
+    title: 'string',
+    description: 'string',
+    status: 'string',  
+    priority: 'string',
+    icon: 'string',
+    category: 'string',
+    startAt: 'date',
+    endAt: 'date',
+};
